54a20bd484ab9cf23af412de4ff7489fa72f1f55
Anshul Vyas <anshul1934@gmail.com>
2018-01-06 09:36:32 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var ingredient1= ["bread","mild cheese","sharp cheese","butter","tomato","garlic"]
+
+var chocolateBars=["snickers" ,"hundred grands", "kitkat" ,"skittles"]
+
+console.log(chocolateBars);
