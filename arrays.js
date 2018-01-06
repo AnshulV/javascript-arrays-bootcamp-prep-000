82a102417b40406694493fccf53bbecbc43fc5e2
Anshul Vyas <anshul1934@gmail.com>
2018-01-06 09:37:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var ingredient1= ["bread","mild cheese","sharp cheese","butter","tomato","garlic
 var chocolateBars=["snickers" ,"hundred grands", "kitkat" ,"skittles"]
 
 console.log(chocolateBars);
+
+function addElementToBeginningOfArray(var array,var element) 
+{
+  var array =
+}
